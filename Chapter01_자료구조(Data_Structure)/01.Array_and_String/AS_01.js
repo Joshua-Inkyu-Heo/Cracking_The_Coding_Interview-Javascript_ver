923dc4b65df396980b084933d36df71451b0d358
Joshua <hwhale1011@gmail.com>
2016-08-17 15:15:46 +0900

    Solve AS_02

--- Chapter01_자료구조(Data_Structure)/01.Array_and_String/AS_01.js
+++ Chapter01_자료구조(Data_Structure)/01.Array_and_String/AS_01.js
@@ -1,20 +1,20 @@
 /*
- 1.1 문제
+  1.1 문제
 
- 문자열에 포함된 문자들이 전부 유일한지를 검사하는 알고리즘.
- 다른 자료구조를 사용할 수 없는 상황.
+  문자열에 포함된 문자들이 전부 유일한지를 검사하는 알고리즘.
+  다른 자료구조를 사용할 수 없는 상황.
 */
 
 /*
- 1.1 해답
+  1.1 해답
 
- 1. Bouble Sort 처럼 각 글자를 모든 글자와 대응해 보는 방법.
-    O(n^2)의 시간복잡도 예상.
+  1. Bouble Sort 처럼 각 글자를 모든 글자와 대응해 보는 방법.
+     O(n^2)의 시간복잡도 예상.
 
- 2. 한 글자만 pop 해서 따로 저장하고 남은 문자들을 indexOf로 비교해 보는 방법.
+  2. 한 글자만 pop 해서 따로 저장하고 남은 문자들을 indexOf로 비교해 보는 방법.
 
- 3. Object 이용해서 각 글자를 key로 만들어 중복확인.
-    이 방법으로 풀이.
+  3. Object 이용해서 각 글자를 key로 만들어 중복확인.
+     이 방법으로 풀이.
 */
 
 'use strict';
@@ -40,3 +40,4 @@ let hasUniqueChar = function( str )
 console.log( hasUniqueChar( 'abcdefg' ) );
 console.log( hasUniqueChar( 'abcdefga' ) );
 console.log( hasUniqueChar( 'ajfiegnrie' ) );
+
---
+++ Chapter01_자료구조(Data_Structure)/01.Array_and_String/AS_02.js
@@ -0,0 +1,64 @@
+/*
+  1.2 문제
+
+  널 문자로 끝나는 문자열을 뒤집는 함수를 C나 C++로 구현하라.
+*/
+
+/*
+  1.2 해답
+
+  1. 문제의 의도는 포인터를 서로 교체하면서 뒤집는 방식.
+     Javascript의 경우 argument 문자열 값이 하나의 배열이기 때문에,
+     문자열의 length/에서 0까지 반복출력한다.
+     너무 단순한 풀이라 제외.
+
+  2. 동일 배열에서 글자들의 위치만 변경할 수 는 없음.
+     scope 문제로 인해 새로운 property 만드는 방법으로 풀이.
+*/
+
+'use strict';
+
+// 배열로 테스트
+// let reverseString = function( str )
+// {
+//   let i = 0;
+//   let temp;
+//
+//   for( ; i < str.length / 2; i++ )
+//   {
+//     temp = str[ i ];
+//     str[ i ] = str[ str.length - 1 - i ];
+//     str[ str.length - 1 - i ] = temp;
+//   }트
+//   return str;
+// };
+//
+// console.log( reverseString( [1,2,3,4,5] ) );
+// console.log( reverseString( [1,2,3,4,5,6] ) );
+
+let reverseString = function( str )
+{
+  str = str.split('');
+  //scope 문제로 새로운 property 만듬
+  str.swap();
+  str = str.join('');
+  return str;
+};
+
+Array.prototype.swap = function()
+{
+  let i = 0;
+  let temp;
+
+  for( ; i < this.length / 2; i++ )
+  {
+    temp = this[ i ];
+    this[ i ] = this[ this.length - 1 - i ];
+    this[ this.length - 1 - i ] = temp;
+  }
+  return this;
+};
+
+console.log( reverseString( 'abcde' ) );
+console.log( reverseString( 'abcdef' ) );
+
