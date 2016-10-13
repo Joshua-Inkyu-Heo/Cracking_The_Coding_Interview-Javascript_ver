3e6dbb942e6677576f86cf1e7d0efec161c76808
Joshua <hwhale1011@gmail.com>
2016-10-13 23:55:56 +0900

    Construct basic binary search tree with inorder, preorder, postorder

--- Chapter01_자료구조(Data_Structure)/04.Tree_and_Graph/TG_00.js
+++ Chapter01_자료구조(Data_Structure)/04.Tree_and_Graph/TG_00.js
@@ -1,4 +1,6 @@
+// ---------------------------------------------------------------
 // Basic Tree and Graph with BFS, DFS
+// ---------------------------------------------------------------
 
 'use strict';
 
@@ -123,27 +125,257 @@ let leaf5 = branch2.addChild( 5 );
 let leaf6 = branch3.addChild( 6 );
 let leaf7 = branch3.addChild( 7 );
 
-console.log( root1.DFSelect( function( value , depth )
-{
-  return value % 2;
-} ) );
-// [1, 5, 3, 7]
+// console.log( root1.DFSelect( function( value , depth )
+// {
+//   return value % 2;
+// } ) );
+// // [1, 5, 3, 7]
 
-console.log( root1.DFSelect( function( value , depth )
-{
-  return depth === 1;
-} ) );
-// [2, 3]
+// console.log( root1.DFSelect( function( value , depth )
+// {
+//   return depth === 1;
+// } ) );
+// // [2, 3]
 
-console.log( root1.BFSelect( function( value , depth )
-{
-  return value % 2;
-} ) );
-// [1, 3, 5, 7]
+// console.log( root1.BFSelect( function( value , depth )
+// {
+//   return value % 2;
+// } ) );
+// // [1, 3, 5, 7]
 
-console.log( root1.BFSelect( function( value , depth )
+// console.log( root1.BFSelect( function( value , depth )
+// {
+//   return depth === 1;
+// } ) );
+// // [2, 3]
+
+
+// ---------------------------------------------------------------
+// Basic Binary Search Tree with inorder, preorder, postorder
+// ---------------------------------------------------------------
+
+'use strict';
+
+let Node = function( data , left , right )
 {
-  return depth === 1;
-} ) );
-// [2, 3]
+  this.data = data;
+  this.left = left;
+  this.right = right;
+};
+
+Node.prototype.show = function()
+{
+  return this.data;
+};
+
+let BinartSearchTree = function()
+{
+  this.root = null;
+};
+
+BinartSearchTree.prototype.insert = function( data )
+{
+  let node = new Node( data , null , null );
+
+  if( this.root === null )
+  {
+    this.root = node;
+  }
+  else
+  {
+    let current = this.root;
+    let parent;
+    while( true )
+    {
+      parent = current;
+      if( data < current.data )
+      {
+        current = current.left;
+        if( current === null )
+        {
+          parent.left = node;
+          break;
+        }
+      }
+      else
+      {
+        current = current.right;
+        if( current === null )
+        {
+          parent.right = node;
+          break;
+        }
+      }
+    }
+  }
+};
+
+BinartSearchTree.prototype.inOrder = function( node )
+{
+  if( node !== null )
+  {
+    this.inOrder( node.left );
+    console.log( node.show() + " " );
+    this.inOrder( node.right );
+  }
+};
+
+BinartSearchTree.prototype.preOrder = function( node )
+{
+  if( node !== null )
+  {
+    console.log( node.show() + " " );
+    this.preOrder( node.left );
+    this.preOrder( node.right );
+  }
+};
+
+BinartSearchTree.prototype.postOrder = function( node )
+{
+  if( node !== null )
+  {
+    this.postOrder( node.left );
+    this.postOrder( node.right );
+    console.log( node.show() + " " );
+  }
+};
+
+BinartSearchTree.prototype.getMin = function()
+{
+  let current = this.root;
+  while( current.left !== null )
+  {
+    current = current.left;
+  }
+  return current.data;
+};
+
+BinartSearchTree.prototype.getMax = function()
+{
+  let current = this.root;
+  while( current.right !== null )
+  {
+    current = current.right;
+  }
+  return current.data;
+};
+
+BinartSearchTree.prototype.find = function( data )
+{
+  let current = this.root;
+  while( current.data !== data )
+  {
+    if( data < current.data )
+    {
+      current = current.left;
+    }
+    else
+    {
+      current = current.right;
+    }
+    if( current === null )
+    {
+      return null;
+    }
+  }
+  return current;
+};
+
+BinartSearchTree.prototype.getSmallest = function( node )
+{
+  if( node.left === null )
+  {
+    return node;
+  }
+  else
+  {
+    return this.getSmallest( node.left );
+  }
+};
+
+BinartSearchTree.prototype.remove = function( data )
+{
+  this.root = this.removeNode( this.root , data );
+};
+
+BinartSearchTree.prototype.removeNode = function( node , data )
+{
+  if( node === null )
+  {
+    return null;
+  }
+  // root와 data 같을 때
+  if( data === node.data )
+  {
+    // child 없을 시
+    if( node.left === null && node.right === null )
+    {
+      return null;
+    }
+    // left child 없을 시
+    if( node.left === null )
+    {
+      return node.right;
+    }
+    // right child 없을 시
+    if( node.right === null )
+    {
+      return node.left;
+    }
+    // 양쪽 chlid 다 있을 시
+    // root가 삭제되고 root자라에 올 값 찾기
+    // root보다 큰 값(right)중에서 가장 작은 값이 root가 되어야 함
+    let tempNode = this.getSmallest( node.right );
+    console.log( "tempNode : " , tempNode );
+    node.data = tempNode.data;
+    node.right = this.removeNode( node.right , tempNode.data );
+    return node;
+  }
+  else if( data < node.data )
+  {
+    node.left = this.removeNode( node.left , data );
+    return node;
+  }
+  else
+  {
+    node.right = this.removeNode( node.right , data );
+    return node;
+  }
+};
+
+let nums = new BinartSearchTree();
+
+nums.insert( 23 );
+nums.insert( 45 );
+nums.insert( 16 );
+nums.insert( 37 );
+nums.insert( 3 );
+nums.insert( 99 );
+nums.insert( 22 );
+
+console.log( "Inorder : " );
+nums.inOrder( nums.root );
+console.log( "Preorder : " );
+nums.preOrder( nums.root );
+console.log( "Postorder : " );
+nums.postOrder( nums.root );
+
+let min = nums.getMin();
+console.log( "The minimum value of the BinartSearchTree is: " + min );
+let max = nums.getMax();
+console.log( "The maximum value of the BinartSearchTree is: " + max );
+
+let value = 99;
+let found = nums.find( value );
+if( found !== null )
+{
+  console.log( "Found " + value + " in the BinartSearchTree." );
+}
+else
+{
+  console.log( value + " was not found in the BinartSearchTree." );
+}
+
+nums.remove( 99 );
+console.log( nums );
 
